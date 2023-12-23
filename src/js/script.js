57ad527ec3701700a72e239106b4898a5a37672f
Guilherme silva <guilherme.gui.alves@hotmail.com>
2023-12-22 23:47:54 -0300

    Concluido

---
+++ src/js/script.js
@@ -0,0 +1,13 @@
+var btn = document.querySelector(".no");
+var position
+btn.addEventListener("click", function() {
+position ? (position = 0) : (position =     150);
+btn.style.transform =    `translate(${position}px,0px)`;
+btn.style.transition = "all 0.3s ease";
+});
+function tanks() {
+alert("tanks : Ich liebe dich, 愛してる, I love you o casamento já está marcado)")
+}  
+function not() {
+document.getElementsByClassName('no').style.translateX = ("150px");
+}
